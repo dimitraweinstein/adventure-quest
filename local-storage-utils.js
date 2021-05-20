307635694e156419f32020417e8218d09c8ad51a
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-05-20 16:01:00 -0700

    wrote two more functions, created results file structure

--- local-storage-utils.js
+++ local-storage-utils.js
@@ -1,3 +1,5 @@
+import quests from '../data/quest-data.js';
+
 const PLAYER = 'PLAYER';
 
 export function getPlayer() {
@@ -38,6 +40,23 @@ export function completeQuest(questId) {
     const player = getPlayer();
     
     player.completed[questId] = true;
-    
+
     setPlayer(player);
 }
+
+export function playerhasCompletedAllQuests() {
+    const player = getPlayer();
+    
+    for (let quest of quests) {
+        if (!player.completed[quest.id]) return false;
+    }
+    return true;
+
+}
+
+export function playerDied() {
+    const player = getPlayer();
+
+    if (player.hp <= 0) return true;
+    return false;
+}   
--- map/map.js
+++ map/map.js
@@ -1,4 +1,5 @@
 import quests from '../data/quest-data.js';
+import { playerhasCompletedAllQuests } from '../local-storage-utils.js';
 
 
 const ul = document.querySelector('#quests');
@@ -13,4 +14,23 @@ for (let quest of quests) {
     li.append(a);
     ul.append(li);
 
+}
+
+if (playerhasCompletedAllQuests()) {
+    window.location = '../results';
+
+    const ul = document.querySelector('#quests');
+
+    for (let quest of quests) {
+
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+
+        a.href = `../quests/?id=${quest.id}`;
+        a.textContent = quest.title;
+
+        li.append(a);
+        ul.append(li);
+
+    }
 }
