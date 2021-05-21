861f7558d9799b5049af0b2269438d77da45547e
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-05-21 14:56:43 -0700

    styled home page and general app styling

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ import { setPlayer } from './local-storage-utils.js';
 
 // import functions and grab DOM elements
 const form = document.querySelector('#player-form');
+const characterImage1 = document.querySelector('#yoshi-image');
+const characterImage2 = document.querySelector('#luigi-image');
+const characterImage3 = document.querySelector('#princess-image');
+const characterImage4 = document.querySelector('#toadette-image');
+
+characterImage1.src = 'assets/yoshi-img.png';
+characterImage2.src = 'assets/luigi-img.png';
+characterImage3.src = 'assets/peach-img.png';
+characterImage4.src = 'assets/toadette-img.png';
 
 // set event listeners to update state and DOM
 form.addEventListener('submit', (e) => {
